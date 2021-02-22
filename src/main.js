0b0c9568872acb598e7d87803f87dad80ea7b2e0
li <2681291959@qq.com>
2021-02-22 16:27:36 +0800

    轮播图学习

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+//轮播图
+import carousel from "@/components/carousel.vue";
+Vue.component("carousel", carousel);
+
 Vue.config.productionTip = false;
 
 new Vue({
@@ -10,3 +14,16 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+Vue.use(ElementUI);
+
+new Vue({
+  el: "#app",
+  router,//引入element后添加一个这个，还需注意在用脚手架构建时，vue要选择2.x的版本，不能3.x
+  render: h => h(App)
+});
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <carousel :urls="url"></carousel>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import {getUrl} from "@/api/get.js"
+// import carousel1 from "@/components/carousel.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      url: [],
+      image: []
+    }
+  },
+  created() {
+    this.Url()//在页面加载时，获取轮播图所需要的
+  },
+  methods: {
+    //得到轮播图数据
+    Url() {
+      getUrl().then(infoUrl => {
+        console.log(infoUrl.data.data.d)
+        this.url = infoUrl.data.data.d;
+      })
+    }
   }
 };
 </script>
